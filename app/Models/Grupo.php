5386bd48575e0d23981c3a9f759a8e658e15f815
JairAguilar04 <aguilar.jair.2001@gmail.com>
2024-03-14 13:09:25 -0600

    Factory y Seeder para Area, Grupo y Subarea

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = ['nombre', 'status'];
+
+    public function subAreas()
+    {
+        return $this->hasMany(Subarea::class, 'grupo_id');
+    }
 }
