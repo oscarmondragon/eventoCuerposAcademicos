194fab05b7cb5d957bc66e32753583e350574f51
JairAguilar04 <aguilar.jair.2001@gmail.com>
2024-04-24 11:08:57 -0600

    Mejora en estilos para los campos input file
    se corrigio el mensaje de validacion para "areaSeleccionada", ajuste para los botones en el responsive

--- app/Livewire/Participantes/AdjuntarBoucherCorreo.php
+++ app/Livewire/Participantes/AdjuntarBoucherCorreo.php
@@ -26,7 +26,7 @@ class AdjuntarBoucherCorreo extends Component
     #[Validate('required_unless:boucher,null')]
     public $checkFactura;
 
-    #[Validate('required_if:checkFactura,1||mimes:jpg,pdf,png|max:2048')]
+    #[Validate('nullable|required_if:checkFactura,1|mimes:pdf|max:2048')]
     public $csf;
 
 
@@ -37,9 +37,9 @@ class AdjuntarBoucherCorreo extends Component
 
         'checkFactura.required_unless' => 'Indica si requieres factura.',
 
-        'csf.max' => 'El archivo debe pesar máximo 2 MB.',
+        'csf.max' => 'El archivo CSF debe pesar máximo 2 MB.',
         'csf.required_if' => 'No ha seleccionado ningun archivo.',
-        'csf.mimes' => 'El archivo debe ser de tipo: jpg, pdf, png.',
+        'csf.mimes' => 'El archivo CSF debe ser de tipo: .pdf.',
 
     ];
 
--- resources/views/livewire/participantes/adjuntar-boucher-correo.blade.php
+++ resources/views/livewire/participantes/adjuntar-boucher-correo.blade.php
@@ -8,21 +8,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-10 dark:text-gray-100">
-                    <div id="accordion-open-body-4"" aria-labelledby="accordion-open-heading-4">
+                    <div id="accordion-open-body-4" aria-labelledby="accordion-open-heading-4">
                         <form wire:submit="save">
                             @csrf
                             <div class="p-5 border border-dorado dark:border-gray-700">
-
-                                <div class="flex sm:flex-row flex-col sm:gap-x-8 gap-y-8">
+                                <div>
+                                    <h2>Correo electronico registrado:
+                                        <span
+                                            class="font-bold underline underline-offset-8 decoration-2 decoration-verde">{{ $this->registroFound->email }}</span>
+                                    </h2>
+                                    <h2 class="mt-2">Nombre del cuerpo académico, red o grupo de
+                                        investigación:
+                                        <span
+                                            class="font-bold underline underline-offset-8 decoration-2 decoration-verde">
+                                            {{ $this->registroFound->cuerpo_grupo_red }}
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="flex sm:flex-row flex-col sm:gap-x-8 gap-y-8 items-center mt-5">
                                     <div class="basis-1/2">
-                                        <h2><strong>Correo electronico registrado:</strong>
-                                            {{ $this->registroFound->email }}
-                                        </h2>
-                                        <h2><strong>Nombre del cuerpo académico, red o grupo de investigación :</strong>
-                                            {{ $this->registroFound->cuerpo_grupo_red }}</h2>
-                                        <div class="mt-5">
-                                            <p><strong>Datos para realizar pago:</strong></p>
-                                            <div class="bg-[#003a7c] w-full h-72 rounded-xl mt-5 p-4 text-white">
+                                        <div>
+                                            <h1 class="font-bold">Datos para realizar pago:</h1>
+                                            <div
+                                                class="bg-[#003a7c] w-full sm:h-72 h-auto rounded-xl mt-5 p-4 text-white">
                                                 <h1 class="text-2xl">BBVA Bancomer</h1>
                                                 <h2 class="mt-4">Nombre:
                                                     <span
@@ -62,18 +70,35 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="basis-1/2">
-                                        <label class="block mb-2 mt-5 dark:text-white" for="boucher">Subir
+                                    <div class="basis-1/2 mt-8">
+                                        <label class="block mb-2 dark:text-white" for="boucher">Subir
                                             comprobante de pago</label>
-                                        <input
-                                            class="block w-full text-sm border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                            aria-describedby="form.boucher_help" id="boucher" type="file"
-                                            accept=".jpg,.png, .pdf" wire:model.live="boucher">
+                                        <div class="bg-transparent h-10 flex items-center rounded-l-lg">
+                                            <div>
+                                                <input aria-describedby="form.boucher_help" id="boucher"
+                                                    type="file" accept=".jpg,.png, .pdf" wire:model.live="boucher">
+                                            </div>
+                                            <div class="flex items-center bg-gray-200 h-10 w-full rounded-r-md -ml-1">
+                                                @empty($boucher)
+                                                    <label for="boucher"
+                                                        class="text-textos sm:text-base text-sm pl-2 cursor-pointer">
+                                                        Sin archivos seleccionados.
+                                                    </label>
+                                                @endempty
+                                                @empty(!$boucher)
+                                                    <label for="boucher"
+                                                        class="text-textos sm:text-base text-sm pl-2 cursor-pointer">
+                                                        {{ $boucher->getClientOriginalName() }}</label>
+                                                @endempty
+                                            </div>
+                                        </div>
 
+                                        <div wire:loading wire:target="boucher">
+                                            <span class="text-sm text-textos">Cargando documento...</span>
+                                        </div>
                                         @error('boucher')
                                             <span class="text-rojo block">{{ $message }}</span>
                                         @enderror
-                                        <div wire:loading wire:target="boucher">Cargando documento...</div>
                                         <div class="mt-1 text-sm text-gray-500 dark:text-gray-300"
                                             id="form.boucher_help">
                                             <span class="font-bold">Importante:</span> Es necesario subir tu
@@ -83,19 +108,19 @@
 
                                         @if ($boucher != null)
                                             <div class="mt-4">
-                                                <label for="checkFactura" class="label-modal">
+                                                <label for="si" class="block mb-2 mt-5">
                                                     ¿Requieres factura?<span class="font-bold text-red-600">*</span>
                                                 </label>
                                                 <div class="flex gap-x-5">
                                                     <div>
-                                                        <input type="radio" id="checkFactura" name="checkFactura"
+                                                        <input type="radio" id="si" name="checkFactura"
                                                             wire:model.live="checkFactura" value="1">
-                                                        <label for="checkFactura" class="ml-2 text-textos">Si</label>
+                                                        <label for="si" class="ml-2 text-textos">Si</label>
                                                     </div>
                                                     <div>
-                                                        <input type="radio" id="checkFactura" name="checkFactura"
+                                                        <input type="radio" id="no" name="checkFactura"
                                                             wire:model.live="checkFactura" value="0">
-                                                        <label for="checkFactura" class="ml-2 text-textos">No</label>
+                                                        <label for="no" class="ml-2 text-textos">No</label>
                                                     </div>
                                                 </div>
                                                 @error('checkFactura')
@@ -107,33 +132,48 @@
                                             <div class="mt-4">
                                                 <label class="block mb-2 dark:text-white" for="csf">Subir
                                                     Constancia de Situación Fiscal</label>
-                                                <input
-                                                    class="block w-full text-sm border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                                    aria-describedby="form.csf_help" id="csf" type="file"
-                                                    accept=".pdf" wire:model.live="csf">
-                                                <div wire:loading wire:target="csf">Cargando
-                                                    archivo...
-                                                </div>
-                                                @error('csf')
-                                                    <span class="text-rojo block">{{ $message }}</span>
-                                                @enderror
+                                                <div class="bg-transparent h-10 flex items-center rounded-l-lg">
+                                                    <div>
+                                                        <input aria-describedby="form.csf_help" id="csf"
+                                                            type="file" accept=".pdf" wire:model.live="csf">
+                                                    </div>
+                                                    <div
+                                                        class="flex items-center bg-gray-200 h-10 w-full rounded-r-md -ml-1">
+                                                    @empty($csf)
+                                                        <label for="csf"
+                                                            class="text-textos sm:text-base text-sm pl-2 cursor-pointer">
+                                                            Sin archivos seleccionados.
+                                                        </label>
+                                                    @endempty
+                                                @empty(!$csf)
+                                                    <label for="csf"
+                                                        class="text-textos sm:text-base text-sm pl-2 cursor-pointer">
+                                                        {{ $csf->getClientOriginalName() }}</label>
+                                                @endempty
                                             </div>
-                                        @endif
+                                        </div>
+                                        <div wire:loading wire:target="csf">
+                                            <span class="text-sm text-textos">Cargando archivo...</span>
+                                        </div>
+                                        @error('csf')
+                                            <span class="text-rojo block">{{ $message }}</span>
+                                        @enderror
                                     </div>
-                                </div>
-
-                                <div class="text-end mt-5">
-                                    <x-primary-button class="ms-3">
-                                        {{ __('Enviar') }}
-                                    </x-primary-button>
-                                </div>
-
+                                @endif
                             </div>
-                        </form>
+                        </div>
+
+                        <div class="text-end mt-5">
+                            <x-primary-button>
+                                {{ __('Enviar') }}
+                            </x-primary-button>
+                        </div>
 
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
+</div>
 </div>
