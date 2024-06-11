a1ff9298caa52ec38fd7e17f1bc003af3bc9c0f3
JairAguilar04 <aguilar.jair.2001@gmail.com>
2024-06-11 11:23:57 -0600

    Se puso la ventana con el spinner para actualizar el estatus y se cambio el color del boton secundario.

--- resources/views/livewire/admin/registro-detail.blade.php
+++ resources/views/livewire/admin/registro-detail.blade.php
@@ -373,7 +373,7 @@
                         </div>
 
                         <div>
-                            <form x-on:submit.prevent="confirmacion">
+                            <form wire:submit="save">
                                 <div>
                                     <label for="estatusSelected" class="block mb-2">
                                         Estatus<span class="font-bold text-red-600">*</span>
@@ -403,9 +403,40 @@
                                     <a href="{{ route('dashboard') }}">
                                         <x-secondary-button>Regresar</x-secondary-button>
                                     </a>
-                                    <x-primary-button @click="confirmacion">Guardar</x-primary-button>
+                                    <button type="button" @click="confirmar" class="btn-success button"
+                                        wire:loading.class="opacity-50" wire:target="save">
+                                        Guardar
+                                    </button>
+                                    <button type="submit" id="btnEnviar" hidden>Enviar</button>
                                 </div>
                             </form>
+                            <div wire:loading wire:target="save">
+                                <div
+                                    class="overflow-y-auto overflow-x-hidden cursor-progress bg-gray-500/40 fixed flex top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    <div class="relative p-2 w-full max-w-md max-h-full">
+                                        <div
+                                            class="relative bg-white rounded-lg shadow-2xl shadow-[#34778A]/50 dark:bg-gray-700">
+                                            <div class="p-4 md:p-5 text-center">
+                                                <svg aria-hidden="true"
+                                                    class="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-[#34778A]"
+                                                    viewBox="0 0 100 101" fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                        fill="currentColor" />
+                                                    <path
+                                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                        fill="currentFill" />
+                                                </svg>
+                                                <h3
+                                                    class="mb-5 mt-4 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                                    Actualizando estatus.
+                                                </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -414,7 +445,7 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-        function confirmacion() {
+        function confirmar() {
             Swal.fire({
                 customClass: {
                     title: 'swal2-title'
@@ -432,7 +463,9 @@
 
             }).then((result) => {
                 if (result.isConfirmed) {
-                    Livewire.dispatch('save');
+                    //Livewire.dispatch('save');
+                    let btn = document.getElementById('btnEnviar');
+                    btn.click();
                 }
             });
         }
